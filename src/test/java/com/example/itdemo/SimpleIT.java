1832f2916c0e0d9be9e61f006b2e4698d0b1ab99
Jonni Nakari <jonni@vaadin.com>
2017-02-07 14:21:39 +0100

    Use integration tests in build, read webdriver from system props

--- src/test/java/com/example/itdemo/SimpleIT.java
+++ src/test/java/com/example/itdemo/SimpleIT.java
@@ -1,10 +1,17 @@
 package com.example.itdemo;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.vaadin.testbench.ElementQuery;
@@ -15,17 +22,33 @@ import com.vaadin.testbench.elements.TextFieldElement;
 
 public class SimpleIT extends TestBenchTestCase {
 
-	private RemoteWebDriver driver;
+	private int jettyPort = 8080;
 
 	@Before
-	public void setup() {
-		driver = new FirefoxDriver();
-		setDriver(driver);
+	public void setup() throws MalformedURLException {
+		String desiredDriver = System.getProperty("webdriver");
+		if (desiredDriver == null || "PhantomJS".equalsIgnoreCase(desiredDriver)) {
+			setDriver(new PhantomJSDriver());
+		}
+		else if("Firefox".equalsIgnoreCase(desiredDriver)) {
+			setDriver(new FirefoxDriver());
+		}
+		else if("RemoteWebDriver".equalsIgnoreCase(desiredDriver)) {
+			new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.internetExplorer());
+		}
+		Properties prop = new Properties();
+		try {
+			prop.load(this.getClass().getClassLoader().getResourceAsStream("config.properties"));
+			String portStr = prop.getProperty("jetty.port");
+			jettyPort = Integer.parseInt(portStr);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	@Test
 	public void testGreeting() {
-		driver.get("http://localhost:8080");
+		getDriver().get("http://localhost:" + jettyPort);
 		TextFieldElement typeyournamehereTextField = $(TextFieldElement.class).caption("Type your name here:").first();
 		typeyournamehereTextField.setValue("foobar");
 		ButtonElement clickMeButton = $(ButtonElement.class).caption("Click Me").first();
@@ -35,7 +58,7 @@ public class SimpleIT extends TestBenchTestCase {
 		LabelElement label1 = labelQuery.first();
 		Assert.assertTrue("Greeting should contain the inputted value 'foobar'", label1.getHTML().contains("foobar"));
 	}
-	
+
 	@After
 	public void teardown() {
 		driver.close();
